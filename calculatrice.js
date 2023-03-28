e4f4dd5472450d85284f28baa1cf69372913eaf3
Paul-Mazaingue <paul.mazaingue@student.junia.com>
2023-03-28 21:10:43 +0200

    partie 3

--- calculatrice.js
+++ calculatrice.js
@@ -1,26 +1,27 @@
-
-function add(value) {
-    let typing = document.getElementById("typing");
-    typing.innerHTML += value;
-}
-
-function calculate() {
-    let typing = document.getElementById("typing");
-    let result = document.getElementById("result");
-    let expression = typing.innerHTML;
-    let answer = eval(expression);
-    result.innerHTML = "Résultat : " + expression + " = " + parseFloat(answer).toPrecision(10);
-    typing.innerHTML = '';
-}
-
-function remove(){
-    let typing = document.getElementById("typing");
-    typing.innerHTML = typing.innerHTML.slice(0, -1);
-}
-
-function clearText() {
-    let typing = document.getElementById("typing");
-    let result = document.getElementById("result");
-    typing.innerHTML = '';
-    result.innerHTML = '';
-}
+class BaseCalculator {
+    constructor() {
+      this.typing = document.getElementById("typing");
+      this.result = document.getElementById("result");
+    }
+  
+    add(value) {
+      this.typing.innerHTML += value;
+    }
+  
+    calculate() {
+      this.expression = this.typing.innerHTML;
+      this.answer = eval(this.expression);
+      this.result.innerHTML =
+        "Résultat : " + this.expression + " = " + parseFloat(this.answer);
+      this.typing.innerHTML = "";
+    }
+  
+    remove() {
+      this.typing.innerHTML = this.typing.innerHTML.slice(0, -1);
+    }
+  
+    clearText() {
+      this.typing.innerHTML = "";
+      this.result.innerHTML = "";
+    }
+  }
